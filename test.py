15a9d6127215ef3a15647a7634b1198eef256178
adrianmaciuc <102796169+adrianmaciuc@users.noreply.github.com>
2022-04-02 23:56:35 +0300

    Modificat point 5
    ,m

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 2. am facut schimbari in local la noi
 3. pe local am facut un branch nou si i-am zis 2aprilie
 4. am creat un nou branch stefchanges
-5. am creat branch stefan
+5. am creat branch norbert
 6. Am sters point 1 din modifcarile de la 7
 7. Am modificat point 6
+8. Am modificat pe web point 5
