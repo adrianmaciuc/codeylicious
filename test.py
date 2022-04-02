23845e9f71197bfb40f5ea0c93c83be3dbb8d854
adrianmaciuc <adrian.maciuc@gmail.com>
2022-04-02 23:49:11 +0300

    am sters poijnt 1

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-1. schimbari la main facut pe web
+
 2. am facut schimbari in local la noi
 3. pe local am facut un branch nou si i-am zis 2aprilie
 4. am creat un nou branch stefchanges
 5. am creat branch stefan
+6. Am sters point 1 din branch stefan
