f4561c4a8cc5edd998681bc087feda8fe5ca6705
adrianmaciuc <adrian.maciuc@gmail.com>
2022-04-04 12:16:05 +0300

    Merge branch 'main' of https://github.com/adrianmaciuc/codeylicious

---
+++ pages/basePage.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+from tests.conftest import Config
+
+
+class BasePage():
+    def __init__(self, Config):
+        if Config.BROWSER_TYPE == "Chrome":
+            webdriver.chrome.service.Service(Config.DRIVER_PATH)
+            self.options = webdriver.ChromeOptions()
+            self.options.add_argument('log-level=3')
+            self.browser = webdriver.Chrome(options=self.options)
+
+        elif Config.BROWSER_TYPE == "Edge": 
+            webdriver.edge.service.Service(Config.DRIVER_PATH)
+            self.options = webdriver.edge.options.Options()
+            self.options.add_argument('log-level=3')
+            self.browser = webdriver.Edge(options=self.options)
+
+    def get_driver(self):
+        return self.browser
--- test.py
+++
@@ -1,5 +0,0 @@
-0 . no data
-1. our first change
-2. second change
-6. modificare de adi
-7 . am modificat direct in main
