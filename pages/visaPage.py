ca11ab48349fa6fe1336003f4e0e2549a92aaf4e
AnubisMRM <anubis_mrm@yahoo.com>
2022-04-05 18:09:39 +0300

    Addressing issue 'visaPage needs a reusable method to fill FROM and TO fields'

--- pages/visaPage.py
+++ pages/visaPage.py
@@ -1,4 +1,6 @@
+from multiprocessing.sharedctypes import Value
 import time
+from tests.conftest import ConfigC
 from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -15,29 +17,33 @@ class VisaPage(BasePage):
     DATE_PICKER = (By.ID, "date")
     SUBMIT_BTN = (By.ID, "submit")
     TODAY = datetime.today().strftime('%d-%m-%Y')
-    # TODAY_PLUS_10 = DATE_PICKER.__getattribute__("value")
-    # URL_SUBMIT = f"https://www.phptravels.net/visa/submit/ro/us/{TODAY_PLUS_10}"
-    URL_SUBMIT = "https://www.phptravels.net/visa/submit/ro/us/15-04-2022"
 
     def load(self):
         self.browser.get(VisaPage.URL)
         self.browser.maximize_window()
-
-    def RO_to_US(self):
+    
+    def countriesFromTo(self,VISA_FROM_COUNTRY, VISA_TO_COUNTRY):
         self.browser.find_element(*VisaPage.FROM_COUNTRY_DROPDOWN).click()
-        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys("Romania")
+        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(VISA_FROM_COUNTRY)
         self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(Keys.ENTER)
         self.browser.find_element(*VisaPage.TO_COUNTRY_DROPDOWN).click()
-        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys("United")
-        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(Keys.ARROW_DOWN)
-        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(Keys.ARROW_DOWN)
-        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(Keys.ARROW_DOWN)
+        self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(VISA_TO_COUNTRY)
         self.browser.find_element(*VisaPage.COUNTRY_INPUT).send_keys(Keys.ENTER)
         self.browser.find_element(*VisaPage.SUBMIT_BTN).click()
-    
+
     def close(self):
         self.browser.close()
     
     def screenshot(self):
         date_time = time.strftime("%a.%d.%b.%Y.%H%M%S")
         self.browser.save_screenshot(f"resources\\screenshots\\{date_time}.png")
+
+    def getAttrValue(self, element, atribut):
+        return self.browser.find_element(*element).get_attribute(atribut)
+
+
+if __name__ == "__main__":
+    visapage = VisaPage(ConfigC)
+    visapage.load()
+    valoare = visapage.getAttrValue(VisaPage.DATE_PICKER, "value")
+    visapage.countriesFromTo(ConfigC.VISA_FROM_COUNTRY, ConfigC.VISA_TO_COUNTRY)
