ee06abcfa657966250304fc6d8d59d8964b74d09
kaszonyn <kaszonyi.norbert@gmail.com>
2022-04-07 13:15:20 +0300

    am bagat Hotels code

--- tests/test_hotels.py
+++ tests/test_hotels.py
@@ -7,6 +7,28 @@ from pages.hotelsPage import HotelsPage
 
     # Norbert
 
-class Test_Flights:
+class Test_Hotels:
     def setup(self):
-        self.flights = HotelsPage(ConfigC)
+        self.hotels = HotelsPage(ConfigC)
+
+
+    def booking_hotels(self):
+        self.hotels.load()
+    
+    def test_search_city(self):
+        self.hotels.search_city('Cluj-Napoca')
+        
+    def test_checkin(self):    
+        self.hotels.date_picker_checkin('15')
+
+    def test_checkout(self):
+        self.hotels.date_picker_checkout('25')
+
+    def test_travellers(self):
+        self.hotels.travellers_choice(HotelsPage.ADULTS)
+        
+    def test_travellers_nationality(self):
+        self.hotels.travellers_nationality(HotelsPage.NATIONALITIES[3])
+
+    def teardown(self):
+        self.hotels.close()
